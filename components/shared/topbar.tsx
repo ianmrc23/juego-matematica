31cab0373bd6299c547484df3f17114959e667a1
ianmrc23 <mc3winenter@gmail.com>
2024-05-13 19:02:57 -0400

    avatar de la rata

--- components/shared/topbar.tsx
+++ components/shared/topbar.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { ModeToggle } from "../theme-toggle-button";
+import Image from "next/image";
 
 const Topbar = () => {
   return (
     <header>
       <nav className="flex justify-between items-center">
+        <div className="rounded-sm">
+          <Image alt="avatar"  src={"/public/assest/nelson.jpeg"} width={20} height={20} />
+        </div>
         <h1>Nelson la rata de alcantarilla PortFolio</h1>
         <ModeToggle />
       </nav>
